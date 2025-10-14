0cebcc9032b8d95e475795b4915c31203b154668
djovanovic03 <jovanovicdanilo919@gmail.com>
2025-10-14 20:49:50 +0200

    Added bootstrap, router

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('icr-2025');
+  protected readonly title = signal('Moja aplikacija');
 }
