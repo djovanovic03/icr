7f2173366d5ab380faf99bf3f26936a635dfd3c1
djovanovic03 <jovanovicdanilo919@gmail.com>
2025-10-15 15:23:41 +0200

    Added login and logout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { UserService } from '../services/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,30 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 })
 export class App {
   protected year = new Date().getFullYear()
+
+  constructor(protected router: Router) {}
+
+  getUserName() {
+    const user = UserService.getActiveUser()
+    return `${user.firstName} ${user.lastName}`
+  }
+
+  hasAuth() {
+    return UserService.hasAuth()
+  }
+
+  doLogout() {
+    Swal.fire({
+      title: "Are you sure you want to logout?",
+      
+      showCancelButton: true,
+      confirmButtonText: "Logout now",
+      cancelButtonText: `Don't logout`
+    }).then((result) => {
+      if (result.isConfirmed) {
+        UserService.logout()
+        this.router.navigateByUrl('/')
+      }
+    })
+  }
 }
