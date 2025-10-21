eabbccfa5fb40d28e50b15ce4f2b3fa6574863da
djovanovic03 <jovanovicdanilo919@gmail.com>
2025-10-21 21:06:44 +0200

    Home updates

--- src/services/movie.service.ts
+++ src/services/movie.service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { MovieModel } from "../models/movie.model";
 
 export class MovieService{
-    static async getMovies() {
-        return axios.get<MovieModel[]>('https://movie.pequla.com/api/movie')
+    static async getMovies(search: string = '') {
+        return axios.get<MovieModel[]>(`https://movie.pequla.com/api/movie?search=${search}`)
     }
 }
